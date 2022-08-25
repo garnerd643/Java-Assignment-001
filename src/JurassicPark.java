3f3a49e2cee070352798675ab144a5bd3cf1d5df
netlab-student <garnerd643@gmail.com>
2022-08-25 13:57:01 -0700

    Finished assignment 1!!!

--- src/JurassicPark.java
+++ src/JurassicPark.java
@@ -1,9 +1,20 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Daniel Garner
+ *
+ * @since Version 1.0
+ *
+ */
 public class JurassicPark {
     /**
      * Prints famous Jurasic Park quote to console with only 1 sout statement.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
-        // Write your program here
+        System.out.println("""
+        Life, uh,
+        finds a way
+        -Ian Malcom""");
     }
 }
